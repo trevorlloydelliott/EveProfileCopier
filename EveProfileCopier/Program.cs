485b0df478a7a197805271d385f33a3b449cbadc
Trevor Elliott <trevore@q4websystems.com>
2019-07-16 01:15:00 -0400

    Add project files.

---
+++ EveProfileCopier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EveProfileCopier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
